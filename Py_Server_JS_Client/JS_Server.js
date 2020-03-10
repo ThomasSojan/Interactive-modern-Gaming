8b29c8177c5cc2b23948dbc3b20da82a278a7d73
Thomas T Sojan <thomastsojan97@gmail.com>
2020-03-10 19:20:57 +0530

    JS server added

---
+++ Py_Server_JS_Client/JS_Server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const port = 8080;
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/indexSensor.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('message', function (data) {
+    console.log(data);
+    });
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+
+http.listen(port, function(){
+  console.log(`listening on *:${port}`);
+});
